2fa0ce46b61fe6fdc1c0602d1128b24b50e238da
4ertan <ertanosmanoglu13@gmail.com>
2023-02-01 22:23:24 +0300

    Katmanlar oluşturuldu

---
+++ Entities/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class User
+    {
+        
+        public int UserID { get; set; }
+        
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public DateTime?  Birthdate { get; set; }
+        public DateTime?  CreateDate { get; set; }
+
+        public Role Role { get; set; }
+        public Gender Gender { get; set; }
+        public string Password { get; set; }
+        public UserDetail userDetail { get; set; }
+    }
+}
